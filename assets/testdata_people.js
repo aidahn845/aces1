839f85dc6ebb3d9e191b23e64436b8a518a8b869
aidanh5 <58790190+aidahn845@users.noreply.github.com>
2020-01-30 16:06:54 -0500

    initial commit

---
+++ assets/testdata_people.js
@@ -0,0 +1,162 @@
+var DATA_PEOPLE = [
+  {
+    "personid": 1,
+    "name": "Jeremy Dilmore",
+    "title": "TSM&O Engineer with FDOT district 5",
+    "phone": "",
+    "email": "Jeremy.Dilmore@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 2,
+    "name": "Charles Ramdatt",
+    "title": "Director of Special Projects with the City of Orlando",
+    "phone": "(407) 246-3186",
+    "email": "charles.ramdatt@cityoforlando.net",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 3,
+    "name": "Autumn Young",
+    "title": "",
+    "phone": "",
+    "email": "Autumn.Young@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 4,
+    "name": "Yamilet Diaz",
+    "title": "",
+    "phone": "",
+    "email": "Yamilet.Diaz@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 5,
+    "name": "Amy DiRusso",
+    "title": "",
+    "phone": "",
+    "email": "Amy.Dirusso@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 6,
+    "name": "Robb Jenkins",
+    "title": "",
+    "phone": "",
+    "email": "Robb.Jenkins@go-beep.com",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 7,
+    "name": "Raj Ponnaluri",
+    "title": "",
+    "phone": "",
+    "email": "Raj.Ponnaluri@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 8,
+    "name": "Susan Chrzan",
+    "title": "",
+    "phone": "",
+    "email": "info@tampa-xray.com",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 9,
+    "name": "Katina Kavouklis",
+    "title": "",
+    "phone": "",
+    "email": "Katina.Kavouklis@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 10,
+    "name": "Paul Satchfield",
+    "title": "",
+    "phone": "",
+    "email": "Paul.satchfield@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 11,
+    "name": "Ronald Chin",
+    "title": "",
+    "phone": "",
+    "email": "Ronald.Chin@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 12,
+    "name": "Peter Vega",
+    "title": "",
+    "phone": "",
+    "email": "Peter.Vega@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 13,
+    "name": "Noemi Bonilla",
+    "title": "",
+    "phone": "",
+    "email": "Noemi.Bonilla@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 14,
+    "name": "Pruthvi Manjunatha",
+    "title": "",
+    "phone": "",
+    "email": "pruthvim@ufl.edu",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 15,
+    "name": "Charles Wetzel",
+    "title": "",
+    "phone": "",
+    "email": "Charles.Wetzel@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  },
+  {
+    "personid": 16,
+    "name": "Jesus Gomez",
+    "title": "",
+    "phone": "",
+    "email": "Jesus.Gomez@dot.state.fl.us",
+    "facebook": "",
+    "twitter": "",
+    "linkedin": ""
+  }
+];
---
+++ javascripts/helpers.js
@@ -0,0 +1,12 @@
+// zoom levels from 0, meters / pixel
+const mapboxZP20 = [73551,36775,18387,9194,4597,2298,1149,575,287,144,72,36,18,9,4.5,2.2,1.1];
+const flHeight = 721000;
+const flWidth = 582000;
+
+const STATUS_PLAN = 1;
+const STATUS_DESIGN = 2;
+const STATUS_IMPLEMENT = 3;
+const STATUS_LIVE = 4;
+const STATUS_ARCHIVE = 5;
+
+const STATUS_COLORS = ['#9A00F0', '#FFD417', '#7373ff', '#00FFA8', '#a3a3a3'];
